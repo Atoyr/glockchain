53daf073c7c86cc65b10b2b0a597dec074fa5241
Atoyr <lessironglance@gmail.com>
2018-10-02 00:04:40 +0900

    add blockprint

--- core/cli.go
+++ core/cli.go
@@ -15,7 +15,7 @@ func (cli *CLI) printExecute() {
 	fmt.Println("  / __ \\")
 	fmt.Println(" / / __")
 	fmt.Println("/ /_/  |")
-	fmt.Println("\\__/|_|")
+	fmt.Println("\\___/|_|")
 }
 
 func (cli *CLI) printUsage() {
@@ -31,16 +31,34 @@ func (cli *CLI) Run() {
 	cli.validateArgs()
 	cli.printExecute()
 	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
-
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	var err error
 	switch os.Args[1] {
 	case "addblock":
-		err := addBlockCmd.Parse(os.Args[2:])
-		if err != nil {
-			os.Exit(1)
-		}
+		err = addBlockCmd.Parse(os.Args[2:])
+	case "printchain":
+		err = printChainCmd.Parse(os.Args[2:])
 	default:
 		cli.printUsage()
 		os.Exit(1)
 	}
+	if err != nil {
+		os.Exit(1)
+	}
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
 
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.Bc.Iterator()
+	for {
+		block := bci.Next()
+		fmt.Printf("Hash: %x \n", block.Hash)
+		fmt.Println()
+		if len(block.PreviousHash) == 0 {
+			break
+		}
+	}
 }
