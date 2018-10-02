fb96955eeab5553d7d693004138268427e6f5109
Uchiyama Ryota <r.uchiyama@earth.sint.co.jp>
2018-10-02 12:51:52 +0900

    add base58 encoder utility

---
+++ util/base58.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"math/big"
+)
+
+var base58Char = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+
+func Base58Encode(input []byte) []byte {
+	var result []byte
+	x := big.NewInt(0).SetBytes(input)
+	base := big.NewInt(int64(len(base58Char)))
+	zero := big.NewInt(0)
+	mod := &big.Int{}
+
+	for x.Cmp(zero) != 0 {
+		x.DivMod(x, base, mod)
+		result = append(result, base58Char[mod.Int64()])
+	}
+	if input[0] == 0x00 {
+		result = append(result, base58Char[0])
+	}
+	ReverseBytes(result)
+	return result
+}
+
+func ReverseBytes(data []byte) {
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+}
