7b24806e8e268557572fbd3f248a65c7e0f41617
Uchiyama Ryota <r.uchiyama@earth.sint.co.jp>
2018-10-10 12:51:44 +0900

    mod pubsigkey => pubKey and mod lock

--- core/transactionOutput.go
+++ core/transactionOutput.go
@@ -3,14 +3,14 @@ package core
 import "github.com/atoyr/glockchain/util"
 
 type TXOutput struct {
-	Value     int
-	PubSigKey []byte
+	Value  int
+	PubKey []byte
 }
 
 func (out *TXOutput) Lock(address Address) {
 	pubKeyHash := util.Base58Decode(address.Bytes())
 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
-	out.PubSigKey = pubKeyHash
+	out.PubKey = pubKeyHash
 }
 func NewTXOutput(value int, address Address) *TXOutput {
 	txo := &TXOutput{value, nil}
