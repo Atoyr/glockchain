fc05f84fa8b418d96b9b1db183d158aa907d04cb
Uchiyama Ryota <r.uchiyama@earth.sint.co.jp>
2018-10-12 19:06:38 +0900

    mod txi valiavble

--- core/transactionInput.go
+++ core/transactionInput.go
@@ -7,9 +7,7 @@ import (
 type TXInput struct {
 	PrevTXHash  Hash
 	PrevTXIndex int
-	Address     Address
 	Signature   []byte
-	PubKey      []byte
 }
 
 func NewTXInput(prevTX Transaction, prevTXIndex int) *TXInput {
@@ -20,5 +18,4 @@ func NewTXInput(prevTX Transaction, prevTXIndex int) *TXInput {
 func (txd *TXInput) Lock(address Address) {
 	pubKeyHash := util.Base58Decode(address.Bytes())
 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
-	txd.PubKey = pubKeyHash
 }
