87ac700e07a6d969363668bb7e4da69420c68ac4
Atoyr <lessironglance@gmail.com>
2018-11-01 23:10:18 +0900

    mod Int2Bytes

--- core/transactionInput.go
+++ core/transactionInput.go
@@ -16,7 +16,7 @@ type TXInput struct {
 func (txi *TXInput) Hash() []byte {
 	var b []byte
 	b = append(b, txi.PrevTXHash...)
-	b = append(b, util.Interface2bytes(txi.PrevTXHash)...)
+	b = append(b, util.Int2bytes(txi.PrevTXIndex, 4)...)
 	b = append(b, txi.Signature...)
 	b = append(b, txi.PubKey...)
 	hash := sha256.Sum256(b)
--- core/transactionOutput.go
+++ core/transactionOutput.go
@@ -13,7 +13,7 @@ type TXOutput struct {
 
 func (txo *TXOutput) Hash() []byte {
 	var b []byte
-	b = append(b, util.Interface2bytes(txo.Value)...)
+	b = append(b, util.Int2bytes(txo.Value, 8)...)
 	b = append(b, txo.PubKeyHash...)
 	hash := sha256.Sum256(b)
 	return hash[:]
