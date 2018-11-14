5b7afdc4be98fae0e51aa0be34c4a496ed37d32f
atoyr <lessironglance@gmail.com>
2018-11-14 09:14:00 +0900

    add comment and refactor

--- core/block.go
+++ core/block.go
@@ -13,6 +13,7 @@ import (
 	"github.com/atoyr/glockchain/util"
 )
 
+// Block is transaction block
 type Block struct {
 	Index        int64
 	PreviousHash []byte
@@ -23,22 +24,8 @@ type Block struct {
 	TXHash       []byte
 }
 
-func (block *Block) ToHash() []byte {
-	var b []byte
-	b = append(b, block.PreviousHash...)
-	b = append(b, block.HashTransactions()...)
-	b = append(b, util.Int642bytes(block.Timestamp)...)
-	b = append(b, util.Int2bytes(block.Nonce, 8)...)
-
-	hash := sha256.Sum256(b)
-	return hash[:]
-}
-
-func (b *Block) SetHash() {
-	hash := sha256.Sum256(b.Serialize())
-	b.Hash = hash[:]
-}
-
+// NewBlock is block constructor
+// transactions is the transaction you want to add to the block
 func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
 	var block Block
 	block.Timestamp = time.Now().Unix()
@@ -55,19 +42,36 @@ func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
 	}
 	return &block
 }
+
+// ToHash converts block to hash
+func (block *Block) ToHash() []byte {
+	var b []byte
+	b = append(b, block.PreviousHash...)
+	b = append(b, block.HashTransactions()...)
+	b = append(b, util.Int642bytes(block.Timestamp)...)
+	b = append(b, util.Int2bytes(block.Nonce, 8)...)
+
+	hash := sha256.Sum256(b)
+	return hash[:]
+}
+
+// NewGenesisBlock is created genesis block
+// tx is the coinbase transaction
 func NewGenesisBlock(tx *Transaction) *Block {
 	block := NewBlock([]*Transaction{tx}, []byte{})
 	return block
 }
 
-func (b *Block) Serialize() []byte {
+// Serialize block
+func (block *Block) Serialize() []byte {
 	var result bytes.Buffer
 	encoder := gob.NewEncoder(&result)
-	err := encoder.Encode(b)
+	err := encoder.Encode(block)
 	errorHandle(err)
 	return result.Bytes()
 }
 
+// DeserializeBlock is deserializing block
 func DeserializeBlock(d []byte) *Block {
 	var block Block
 	decoder := gob.NewDecoder(bytes.NewReader(d))
@@ -78,24 +82,27 @@ func DeserializeBlock(d []byte) *Block {
 	}
 	return &block
 }
-func (b *Block) HashTransactions() []byte {
+
+// HashTransactions is hasing transacitons from block
+func (block *Block) HashTransactions() []byte {
 	var txbytes [][]byte
 
-	for _, tx := range b.Transactions {
+	for _, tx := range block.Transactions {
 		txbytes = append(txbytes, tx.Bytes())
 	}
 	mtree := NewMerkleTree(txbytes)
 	return mtree.RootNode.Data
 }
 
-func (b *Block) String() string {
+// String is copnvert block to string
+func (block *Block) String() string {
 	var lines []string
-	lines = append(lines, fmt.Sprintf("Block : %x", b.Hash))
-	lines = append(lines, fmt.Sprintf("index : %d", b.Index))
-	lines = append(lines, fmt.Sprintf("prev : %x", b.PreviousHash))
-	lines = append(lines, fmt.Sprintf("timestamp : %x", b.Timestamp))
-	lines = append(lines, fmt.Sprintf("nonce : %d", b.Nonce))
-	for _, tx := range b.Transactions {
+	lines = append(lines, fmt.Sprintf("Block : %x", block.Hash))
+	lines = append(lines, fmt.Sprintf("index : %d", block.Index))
+	lines = append(lines, fmt.Sprintf("prev : %x", block.PreviousHash))
+	lines = append(lines, fmt.Sprintf("timestamp : %x", block.Timestamp))
+	lines = append(lines, fmt.Sprintf("nonce : %d", block.Nonce))
+	for _, tx := range block.Transactions {
 		lines = append(lines, tx.String())
 	}
 	return strings.Join(lines, "\n")
